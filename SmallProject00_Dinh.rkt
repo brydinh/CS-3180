5e8ced7ae06f19da95de5386b22dda28d2af02b3
Brian Dinh <bdinh98@gmail.com>
2019-09-08 12:56:48 -0400

    Implemented before-in-list function alongside appropiate test cases

--- SmallProject00_Dinh.rkt
+++ SmallProject00_Dinh.rkt
@@ -13,9 +13,12 @@
 ;; DESCRIPTION: dotProduct takes two lists of numbers and produces their dot product
 ;;              (or reports their incompatibility)
 (define (dotProduct l1 l2)
-  (cond [(> (length l1) (length l2)) (displayln "Lists are incompatible")]
-        [(> (length l2) (length l1)) (displayln "Lists are incompatible")]
-        [else (define dotProduct (apply + (map * l1 l2))) (displayln dotProduct)]
+  (cond
+    [(empty? l1) #f]
+    [(empty? l2) #f]
+    [(> (length l1) (length l2)) (displayln "Lists are incompatible")]
+    [(> (length l2) (length l1)) (displayln "Lists are incompatible")]
+    [else (define dotProduct (apply + (map * l1 l2))) (displayln dotProduct)]
   )
 )
 
@@ -29,16 +32,20 @@
 (define (max2 a b)
   (if (> b a) b a))
 
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; FTN Name:    before-in-list? (17.11)
 ;; DESCRIPTION: before-in-list? takes a list and 2 elements of the list. Returns
 ;;              true if the second argument appears before the third argument. 
 (define (before-in-list? l1 e1 e2)
-  (displayln "return true if 2nd appears before 3rd")
-  )
+   (cond
+    [(empty? l1) #f]
+    [(eq? e1 (first l1)) #t]
+    [(eq? e2 (first l1)) #f]
+    [else (before-in-list? (rest l1) e1 e2)]
+    )
+)
 
-(define (removeAll l1 l2)
+(define (removeAll list-a list-b)
   (displayln "help me")
 )
 
@@ -46,8 +53,15 @@
   (displayln "god left me unfinished")
 )
 
+(define (typer nl)
+  (displayln "eskeittt")
+)
+
 ; test cases
-(dotProduct '(1 2) '(3 4))
-(dotProduct '(1 2 3) '(4 5 6))
-(dotProduct '(1 2 3) '(4 5))
-(dotProduct '(1 2 ) '(4 5 6))
+;(dotProduct '(1 2) '(3 4))
+;(dotProduct '(1 2 3) '(4 5 6))
+;(dotProduct '(1 2 3) '(4 5))
+;(dotProduct '(1 2 ) '(4 5 6))
+
+(before-in-list? '(1 2) 1 2)
+(before-in-list? '(2 1) 1 2)
