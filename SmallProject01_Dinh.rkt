696c00ebbe04782def60e9808ef756716f095cbc
Brian Dinh <bdinh98@gmail.com>
2019-09-18 15:56:51 -0400

    Began working on parsing entire string-- not one occurrence/string

--- SmallProject01_Dinh.rkt
+++ SmallProject01_Dinh.rkt
@@ -4,6 +4,8 @@
 
 (define wordLst (read-words "words.txt"))
 
+; TODO: Optimize filters and figure out how to iterate through entire string, not check one occurrence/string
+
 ; Part 1: Reads words.txt and filters out 7 letter words that do not contain the letters e, i, o, or u. Output will be on the same line seperated by commas.
 (define filteredLst (string-join (filter (lambda (x) (eq? (string-length x) 7))
         (filter (lambda (x) (not (string-contains? x "e")))
@@ -30,6 +32,21 @@
 
 (count-of wordLst)
 
+;(define (count-of lst)
+ ; [cond
+  ;  [(null? lst) 0]
+   ; [(string-contains? (car lst) "z") (+ (killme (string->list (car lst))) (count-of (rest lst)))] ; we want to iterate through the entire string to see if multiple occurrences happen
+    ;[(string-contains? (car lst) "Z") (+ (killme (string->list (car lst))) (count-of (rest lst)))] ; we want to iterate through the entire string to see if multiple occurrences happen
+    ;[else (count-of (rest lst))]
+  ;]
+;)
 
-
-; TODO: Optimize filters and function
+;(define (killme word)
+ ; (displayln word)
+  ;[cond
+   ; [(null? word) 0]
+    ;[(char=? (car word) #\z) (+ 1 (killme (rest word)))] ; we want to iterate through the entire string to see if multiple occurrences happen
+    ;[(char=? (car word) #\Z) (+ 1 (killme (rest word)))] ; we want to iterate through the entire string to see if multiple occurrences happen
+    ;[else (count-of (rest word))]
+  ;]
+;)
