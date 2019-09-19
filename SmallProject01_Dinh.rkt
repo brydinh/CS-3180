36a4ca2908504ff5dbe6c608ae7969efb5aebc5b
Brian Dinh <bdinh98@gmail.com>
2019-09-19 13:25:31 -0400

    Added documentation

--- SmallProject01_Dinh.rkt
+++ SmallProject01_Dinh.rkt
@@ -3,21 +3,23 @@
 
 (define wordLst (read-lines "words.txt"))
 
-; TODO: Optimize filters/code
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; CS 3180 Fall '19
+;; Small Project 01
+;; Brian Dinh
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; Part 1: Reads words.txt and filters out 7 letter words that do not contain the letters e, i, o, or u. Output will be on the same line seperated by commas.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Part 1: Reads words.txt and filters out 7 letter words that do not contain
+;;         the letters e, i, o, or u. Output will be on the same line seperated
+;;         by commas. Implemented helper function for letter check.
 
 (define (contains? word)
   (cond
     [(empty? word) #f]
-    [(string-contains? word "e" ) #f]
-    [(string-contains? word "i" ) #f]
-    [(string-contains? word "o" ) #f]
-    [(string-contains? word "u" ) #f]
-    [(string-contains? word "E" ) #f]
-    [(string-contains? word "I" ) #f]
-    [(string-contains? word "O" ) #f]
-    [(string-contains? word "U" ) #f]
+    [(or (string-contains? word "e") (string-contains? word "i") (string-contains? word "o")
+         (string-contains? word "u") (string-contains? word "E") (string-contains? word "I")
+         (string-contains? word "O") (string-contains? word "U")) #f]
     [else #t]
     )
 )
@@ -26,8 +28,12 @@
   (string-join (filter (lambda (x) (eq? (string-length x) 7))
                        (filter (lambda (x) (contains? x)) wordLst)) ", "))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Part 2: Counts how frequent the letters 'Z' and 'z' occurs in words.txt. The
+;;         Count-of function detects if 'Z' or 'z' are present in the string,
+;;         and the helper function count-char counts the total amount of time 'Z'
+;;         or 'z' occurs per string.
 
-; Part 2: Counts how frequent Z and z occurs in wordLst (read from words.txt).
 (define (count-of lst)
   [cond
     [(null? lst) 0]
@@ -41,6 +47,6 @@
   (+ (count (lambda (x) (eq? x #\z)) word) (count (lambda (x) (eq? x #\Z)) word))
 )
 
-
+;; Displays filtered list & number of z occurrences
 (displayln filteredLst)
 (count-of wordLst)
