53394b3bb2347126ce21fd69ae67bc496c41ebde
Brian Dinh <bdinh98@gmail.com>
2019-09-18 23:29:06 -0400

    Accounted for entire string

--- SmallProject01_Dinh.rkt
+++ SmallProject01_Dinh.rkt
@@ -1,10 +1,11 @@
 #lang racket
-
 (require 2htdp/batch-io)
 
-(define wordLst (read-words "words.txt"))
+;(time (begin (read-lines "words.txt") 'done))
 
-; TODO: Optimize filters and figure out how to iterate through entire string, not check one occurrence/string
+(define wordLst (read-lines "words.txt"))
+
+; TODO: Optimize filters/code
 
 ; Part 1: Reads words.txt and filters out 7 letter words that do not contain the letters e, i, o, or u. Output will be on the same line seperated by commas.
 (define filteredLst (string-join (filter (lambda (x) (eq? (string-length x) 7))
@@ -21,32 +22,16 @@
 (define (count-of lst)
   [cond
     [(null? lst) 0]
-    [(string-contains? (car lst) "z") (+ 1 (count-of (rest lst)))]
-    [(string-contains? (car lst) "Z") (+ 1 (count-of (rest lst)))]
+    [(string-contains? (first lst) "z") (+ (count-char (string->list(first lst))) (count-of (rest lst)))] 
+    [(string-contains? (first lst) "Z") (+ (count-char (string->list(first lst))) (count-of (rest lst)))] 
     [else (count-of (rest lst))]
   ]
 )
 
+(define (count-char word)
+  (+ (count (lambda (x) (eq? x #\z)) word) (count (lambda (x) (eq? x #\Z)) word))
+)
 
 (displayln filteredLst)
 
 (count-of wordLst)
-
-;(define (count-of lst)
- ; [cond
-  ;  [(null? lst) 0]
-   ; [(string-contains? (car lst) "z") (+ (killme (string->list (car lst))) (count-of (rest lst)))] ; we want to iterate through the entire string to see if multiple occurrences happen
-    ;[(string-contains? (car lst) "Z") (+ (killme (string->list (car lst))) (count-of (rest lst)))] ; we want to iterate through the entire string to see if multiple occurrences happen
-    ;[else (count-of (rest lst))]
-  ;]
-;)
-
-;(define (killme word)
- ; (displayln word)
-  ;[cond
-   ; [(null? word) 0]
-    ;[(char=? (car word) #\z) (+ 1 (killme (rest word)))] ; we want to iterate through the entire string to see if multiple occurrences happen
-    ;[(char=? (car word) #\Z) (+ 1 (killme (rest word)))] ; we want to iterate through the entire string to see if multiple occurrences happen
-    ;[else (count-of (rest word))]
-  ;]
-;)
