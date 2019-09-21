9f8ce0ca70c353127fe3ea4cdd94f5fb1b85a07c
Brian Dinh <bdinh98@gmail.com>
2019-09-20 20:30:12 -0400

    More initialization

--- Project00_Dinh.py
+++ Project00_Dinh.py
@@ -1,1 +1,18 @@
-print ("HI")
+def find_words(letters)
+'''
+    CONTRACT: find-words : letters -> String
+    PURPOSE: Returns a string of comma delimited dictionary words 7
+    letters long and in alphabetical order that can be composed of
+    characters in letters. (anagrams of letters) There is no trailing
+    comma after the last word in the returned string.
+    Each letter in letters may only be used once per match, e.g.
+    (find-words '("zymomin" "am")) could return "mammoni, zymomin"
+    because "mammoni" is composed of letters in letters including the
+    three 'm' characters in letters, and "zymomin" is similarly composed of
+    letters in letters. However, "mammomi" could not be
+    returned because "mammomi" requires four 'm' characters and
+    only three are available in letters.
+    CODE:
+    '''
+        pass # Put your implementation here instead of pass
+)
