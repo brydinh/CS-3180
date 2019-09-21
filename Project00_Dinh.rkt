1fc88c5166b5949d4589080023497ae03ab13bdf
Brian Dinh <bdinh98@gmail.com>
2019-09-20 20:21:11 -0400

    Initialized Files

---
+++ Project00_Dinh.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+; CONTRACT: find-words : letters -> String
+; PURPOSE: Returns a string of comma delimited dictionary words 7
+; letters long and in alphabetical order that can be composed of
+; characters in letters. (anagrams of letters) There is no trailing
+; comma after the last word in the returned string.
+; Each letter in letters may only be used once per match, e.g.
+; (find-words '("zymomin" "am")) could return "mammoni, zymomin"
+; because "mammoni" is composed of letters in letters including the
+; three 'm' characters in letters, and "zymomin" is similarly composed of
+; letters in letters. However, "mammomi" could not be 
+; returned because "mammomi" requires four 'm' characters and 
+; only three are available in letters.
+; CODE:
+(define (find-words letters)
+   '() ; Put your implementation here instead of returning an empty list
+)
+
