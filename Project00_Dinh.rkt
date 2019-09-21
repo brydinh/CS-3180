fdd82c771f6bc034c5776997298fb40f304211cd
Brian Dinh <bdinh98@gmail.com>
2019-09-20 22:16:42 -0400

    Prototyped initial function. Needs more work

--- Project00_Dinh.rkt
+++ Project00_Dinh.rkt
@@ -1,4 +1,7 @@
 #lang racket
+(require 2htdp/batch-io)
+
+(define wordLst (read-lines "words.txt"))
 
 ; CONTRACT: find-words : letters -> String
 ; PURPOSE: Returns a string of comma delimited dictionary words 7
@@ -13,7 +16,50 @@
 ; returned because "mammomi" requires four 'm' characters and 
 ; only three are available in letters.
 ; CODE:
+
+;TODO Figure out correct logic for contains?
 (define (find-words letters)
-   '() ; Put your implementation here instead of returning an empty list
+  (string-join (filter (lambda (x) (eq? (string-length x) 7))
+                       (filter (lambda (x) (contains? (string->list x) (string->list (string-append* letters)))) wordLst)) ", ")  
 )
+
+; BOTH PASSED ARGUMENTS ARE CHAR LISTS
+(define (contains? list-a list-b) ; A = (a z i m i n o), B = (z y m o m i n o i x a)
+  (cond
+    [(and (empty? list-b) (empty? list-a)) #t]
+    [(and (empty? list-b) (not (empty? list-a))) #f]
+    [else (contains? (filter (lambda (x) (not (equal? (first list-b) x))) list-a) (rest list-b))]
+  )
+)
+
+(define (removeAll list-a list-b)
+  (cond
+    [(empty? list-b) list-a]
+    [else (removeAll (filter (lambda (x) (not (equal? (first list-b) x))) list-a) (rest list-b))]
+  )
+)
+
+; Maximon, monimia, monomya
+; ammonia, amninia, anonyma,
+
+;(removeAll '(a z i m i n o) '(z y m o m i n o i x a))
+;(removeAll '(m a m m o n i) '(z y m o m i n o i x a))
+;(removeAll '(m a x i m i n) '(z y m o m i n o i x a))
+;(removeAll '(m a x i m o n) '(z y m o m i n o i x a))
+;(removeAll '(m i n i m a x) '(z y m o m i n o i x a))
+;(removeAll '(m o n i m i a) '(z y m o m i n o i x a))
+;(removeAll '(m o n o m y a) '(z y m o m i n o i x a))
+;(removeAll '(b y m o m i n) '(z y m o m i n o i x a))
+
+
+
+;(removeAll '(a z i m i n o) '(z y m o m i n o i x a))
+;(removeAll '(a b b c c d) '(a c a))
+
+; displayln (find-words '("zymomin" "omixa"))) should output
+; azimino, mammoni, maximin, maximon, minimax, monimia, monomya, zymomin
+  
+(find-words '("zymomin" "omixa"))
+
+
 
